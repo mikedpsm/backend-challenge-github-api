2302179b5f4d6d95ed943bd424526c8c107e9153
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2023-08-04 05:58:47 -0300

    Fixes controller file name

--- src/routes/githubUserRoutes.ts
+++ src/routes/githubUserRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import githubUserController from '../controller/GithubUserController';
+import githubUserController from '../controller/githubUserController';
 
 const router = express.Router();
 
